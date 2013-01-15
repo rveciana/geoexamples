cf9104c66360b2bebdb2086e57471136d17432d6
rveciana <rveciana@gmail.com>
2013-01-15 20:13:50 +0100

    Removing some useless files + changes in bubbles.html (remove clustering)

--- kartograph-electoral-map/elections.py
+++
@@ -1,8 +0,0 @@
-from kartograph import Kartograph
-from kartograph.options import read_map_descriptor
-import sys
-K = Kartograph()
-css = open("elections.css").read()
-cfg = read_map_descriptor(open("elections.json"))
-K.generate(cfg, outfile='elections.svg', format='svg', stylesheet=css)
-
